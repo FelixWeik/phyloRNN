178cc7dc35076b8767d81aaf4a8bee16be2f3185
Felix <uxhoe@student.kit.edu>
2026-09-14 09:21:33 +0200

    test run on phyml

--- 4a.run_phyml_comparison.py
+++ 4a.run_phyml_comparison.py
@@ -4,7 +4,7 @@ n_sims = 5
 
 # Step 1: Simulate a test set with phyML running
 sim = pn.simulator(
-    n_taxa=20, n_sites=100,
+    n_taxa=28, n_sites=1000,
     freq_uncorrelated_sites=0.5,
     freq_mixed_models=0.05,
     phyml_path=None,  # None as installed system wide
@@ -19,11 +19,11 @@ pn.simulate_parallel(sim, add_day_tag=False)
 # Step 3: Run the comparison
 m, preds, comp_sim, inputs, outputs, results = pn.compare_rnn_phyml(
     compare_file="test_data.npz",
-    model_file="t20_s100",
+    model_file="t28_s1000",
     model_wd="Trained_models",
     output_dir="results/phyml/1",
     rnn_model_tag="",
-    n_taxa=20,
+    n_taxa=28,
     log_rates=False,
     log_tl=True,
     parse_heterogeneity_models=True,  # breakdown by heterogeneity model
@@ -32,4 +32,4 @@ m, preds, comp_sim, inputs, outputs, results = pn.compare_rnn_phyml(
 )
 
 # Step 4: Access the results
-print(results['rates-all'])  # MSE/R² for Gamma, FreeRate, RNN
+print(results['rates-all'])  # MSE/R² for Gamma, FreeRate, and RNN
